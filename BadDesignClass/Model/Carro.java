cc477c4e243e2b60ffcf20a03eeb7524e3167ec5
Gerardo Cordero <cordero.gerard@gmail.com>
2016-04-17 00:40:25 -0430

    Open/Close Principle Example of a class without this principle

--- BadDesignClass/Model/Carro.java
+++ BadDesignClass/Model/Carro.java
@@ -1,9 +1,34 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carro {
 	
+	List<OrdenItem> listOrdenItem = new ArrayList<OrdenItem>();
+	
 	public float MontoTotal(){
-		return 0;
+		float total=0;
+		float descuento=0;
+		for(int i=0;i<listOrdenItem.size();i++){
+			if(listOrdenItem.get(i).getRegla().equals("NORMAL")){
+				total+=listOrdenItem.get(i).getCantidad()*listOrdenItem.get(i).getPrecio();
+				descuento = (float) (total* 0.1);
+				total = total -descuento;
+			}else if(listOrdenItem.get(i).getRegla().equals("PESO")){
+				total+=listOrdenItem.get(i).getCantidad()*listOrdenItem.get(i).getPrecio();
+				descuento = (float) (total* 0.2);
+				total = total -descuento;
+			}
+			else if(listOrdenItem.get(i).getRegla().equals("ESPECIAL")){
+				total+=listOrdenItem.get(i).getCantidad()*listOrdenItem.get(i).getPrecio();
+				descuento = (float) (total* 0.4);
+				total = total -descuento;
+			}
+			//aqui agregarias mas reglas
+		}
+		return total;
+		
 	}
 	public IEnumerable<OrdenItem> Items(){
 		return null;
--- BadDesignClass/Model/OrdenItem.java
+++ BadDesignClass/Model/OrdenItem.java
@@ -2,13 +2,28 @@ package Model;
 
 public class OrdenItem {
 	
-	public String Sku(){
-		return null;
-		
+	private String regla;
+	private float cantidad;
+	private float precio;
+	public String getRegla() {
+		return regla;
 	}
-	public int Cantidad(){
-		return 0;
-		
+	public void setRegla(String regla) {
+		this.regla = regla;
 	}
+	public float getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(float cantidad) {
+		this.cantidad = cantidad;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
 
 }
